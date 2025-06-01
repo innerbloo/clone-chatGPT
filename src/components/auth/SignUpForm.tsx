9071dedbb7027c107c837ff2b1f3372208e36b23
Hosung <nsa19020@naver.com>
2025-06-01 22:22:30 +0900

    회원가입 유효성 검증 로직 추가

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import { ChangeEvent } from 'react';
+
 import { FormCard } from '@/components/auth/FormCard';
+import FormMessage from '@/components/auth/FormMessage';
 import Submit from '@/components/auth/Submit';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useFormValidate } from '@/hooks/useFormValidate';
+import { SignUpSchema } from '@/schemas/auth';
+import { TSignUpFormError } from '@/types/form';
 
 export default function SignUpForm() {
+    const { errors, validateField } =
+        useFormValidate<TSignUpFormError>(SignUpSchema);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        validateField(name, value);
+    };
+
     return (
         <FormCard
             title="회원가입"
@@ -16,7 +32,10 @@ export default function SignUpForm() {
                         id="name"
                         name="name"
                         placeholder="이름을 입력해주세요"
+                        error={!!errors?.name}
+                        onChange={handleChange}
                     />
+                    {errors?.name && <FormMessage message={errors?.name[0]} />}
                 </div>
                 <div className={'space-y-1'}>
                     <Label htmlFor="name">이메일</Label>
@@ -25,7 +44,12 @@ export default function SignUpForm() {
                         name="email"
                         type="email"
                         placeholder="example@example.com"
+                        error={!!errors?.email}
+                        onChange={handleChange}
                     />
+                    {errors?.email && (
+                        <FormMessage message={errors?.email[0]} />
+                    )}
                 </div>
                 <div className={'space-y-1'}>
                     <Label htmlFor="name">비밀번호</Label>
@@ -34,7 +58,12 @@ export default function SignUpForm() {
                         name="password"
                         type="password"
                         placeholder="**********"
+                        error={!!errors?.password}
+                        onChange={handleChange}
                     />
+                    {errors?.password && (
+                        <FormMessage message={errors?.password[0]} />
+                    )}
                 </div>
                 <Submit className={'w-full'}>가입하기</Submit>
             </form>
